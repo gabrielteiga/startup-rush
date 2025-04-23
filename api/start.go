1b5d0190690ea9d00cd4fd83704c6bc4cac0aee6
Gabriel Teiga <gabrielteiga99@gmail.com>
2025-04-23 15:11:37 -0300

    fix: adjusts in CORS configs

--- api/start.go
+++ api/start.go
@@ -12,15 +12,31 @@ var PORT = ":" + configs.APP_PORT
 
 func Start() {
 	mux := http.NewServeMux()
-
 	initEndpoints(mux, routes.Provide())
 
+	handler := corsMiddleware(mux)
+
 	log.Printf("Starting server in the %s port...", PORT)
-	log.Fatal(http.ListenAndServe(PORT, mux))
+	log.Fatal(http.ListenAndServe(PORT, handler))
 }
 
 func initEndpoints(mux *http.ServeMux, controllers map[string]routes.RouteInterface) {
 	for endpoint, controller := range controllers {
 		mux.HandleFunc(endpoint, controller.Handle)
 	}
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
